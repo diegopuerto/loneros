7959943f1ebe2a3c48a39213390c5af87c72ae3a
Vecino Programador <programador@vecino.com.co>
2015-10-10 16:56:02 -0500

    Agrega parámetros adicionales a devise#sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configurar_parametros_permitidos, if: :devise_controller?
+
+  protected
+
+    def configurar_parametros_permitidos
+      devise_parameter_sanitizer.for(:sign_up) << :nombre
+      devise_parameter_sanitizer.for(:sign_up) << :celular
+    end
 end
