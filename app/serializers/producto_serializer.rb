7dc21629913f2ac07d485b15cd61b92cf55b596d
diego <diego.puerto@vecino.com.co>
2015-10-19 16:44:39 -0500

    Agrega precios, imagenes, categorias y caracteristicas al serializador de productos + pruebas_unitarias

--- app/serializers/producto_serializer.rb
+++ app/serializers/producto_serializer.rb
@@ -2,4 +2,11 @@ class ProductoSerializer < ActiveModel::Serializer
   attributes :id,
   	:nombre,
     :descripcion
+
+    
+    #Asociaciones
+    has_many :precios
+    has_many :caracteristicas
+    has_many :imagenes
+    has_many :categorias
 end
